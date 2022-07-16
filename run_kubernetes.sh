eabda7f1adfe38892e6ee6f76986c147bb489c32
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2022-07-16 03:59:33 +0100

    tasks: edit sctipts, include output [kubernetes]

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="bisibabs/mlmapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlmapi --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmapi 8080:80
